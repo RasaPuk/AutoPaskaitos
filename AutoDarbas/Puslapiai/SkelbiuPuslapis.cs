53315c5c615e6913664e498c2a4883087c9dfd73
RasaPuk <77339089+RasaPuk@users.noreply.github.com>
2021-01-16 16:55:33 +0200

    Add files via upload
    ZooBaze testai

---
+++ AutoDarbas/Puslapiai/SkelbiuPuslapis.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPaskaitos.AutoDarbas.Puslapiai
+{
+    class SkelbiuPuslapis
+    {
+    }
+}
---
+++ AutoDarbas/Puslapiai/ZooPuslapiai.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPaskaitos.AutoDarbas.Puslapiai
+{
+    class ZooPuslapiai : BazinePuslapiu3
+    {
+        public ZooPuslapiai(IWebDriver driver): base(driver)
+        {
+            //toliau turi eiti elementai ir metodukai. Pavyzdys - langas 01
+        }
+    }
+}
